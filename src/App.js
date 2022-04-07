eb3a9d380a18a2d7bfedd80ead3f6ade98e7346d
Sathwic Jain <sathwic.j@gmail.com>
2022-04-08 03:49:09 +0530

    done

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ function Home() {
            - it makes both our lives easier! Cheers!
         </div>
         <div className="col-7 col-md-7 image-box">
-            <img className="image" src="mine.jpeg" alt="my image"/>
+            <img className="image" src="mine.jpeg" alt="user"/>
         </div>
         </div>
         </div>
