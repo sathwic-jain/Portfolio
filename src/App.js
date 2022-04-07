61d554afcfe9a03ec9a1166bc204c8a57312d26f
Sathwic Jain <sathwic.j@gmail.com>
2022-04-08 02:19:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import "./App.css";
+import { teal } from "@mui/material/colors";
+import { Projects } from "./Projects";
+import { IoLogoJavascript } from "react-icons/io";
+import { GrReactjs } from "react-icons/gr";
+import { SiMongodb } from "react-icons/si";
+import { DiNodejs, DiCss3 } from "react-icons/di";
+import { AiOutlineHtml5 } from "react-icons/ai";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static" className="bar_style" style={{ color: teal }}>
+        <ul className="appbar">
+          <li>
+            <Link to="/">
+              <Button variant="text" style={{ color: "white" }}>
+                Home
+              </Button>
+            </Link>
+          </li>
+          <li>
+            <Link to="/projects">
+              <Button variant="text" style={{ color: "white" }}>
+                Projects
+              </Button>
+            </Link>
+          </li>
+        </ul>
+      </AppBar>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/projects">
+          <Projects />
+        </Route>
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+function Home() {
+  return (
+    <div>
+      {/* <div className="mainbody">
+        <div className="me">
+          {" "}
+          <h3 className="about">About</h3>A full-stack web developer from
+          Kerala,India looking for professional experience working with cool
+          tech including ReactJS, JavaScript, HTML & CSS and MongoDB. When I'm
+          not coding, you can find me scrolling through Twitter, reading a book
+          or playing some of my favourite video games on PlayStation 5 - you
+          know, stuff I can do indoors. If you wish to reach me through a pretty
+          form, I am sorry to disappoint you as there is none. Just shoot me a
+          mail @ sathwicked00@gmail.com - it makes both our lives easier! Cheers!
+        </div>
+
+        <img className="image" src="screen.jpg" />
+      </div> */}
+      <div className="mainbody">
+        <div className="container text-box">
+          <div className="row">
+          <div className="col-5 col-md-5">
+          {" "}
+          <div style={{textAlign:"center"}}>
+          <h3 className="about">About</h3>
+          </div>
+          <p>A full-stack web developer from
+          Kerala,India looking for professional experience working with cool
+          tech including ReactJS, JavaScript, HTML & CSS and MongoDB.</p>
+          
+           <p>When I'm not coding, you can find me scrolling through Twitter, reading a book
+          or playing some of my favourite video games on PlayStation 5 - you
+          know, stuff I can do indoors.</p>
+          
+          <p> If you wish to reach me through a prettyform, I am sorry to disappoint you as there is none. Just shoot me a
+          mail</p>
+          <p> @ sathwicked00@gmail.com
+          </p>
+           - it makes both our lives easier! Cheers!
+        </div>
+        <div className="col-7 col-md-7 image-box">
+            <img className="image" src="mine.jpeg" />
+        </div>
+        </div>
+        </div>
+      </div>
+      <div className="icons">
+        <h3>
+          <IoLogoJavascript />
+          <GrReactjs />
+          <SiMongodb />
+          <DiNodejs />
+          <AiOutlineHtml5 />
+          <DiCss3 />
+        </h3>
+      </div>
+    </div>
+  );
+}
---
+++ src/Projects.js
@@ -0,0 +1,190 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import {useState} from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { FaGlobe } from "react-icons/fa";
+
+export function Projects() {
+   const [opt,setopt]=useState(0);
+  return (
+    <div className="projects">
+      <div className="card">
+      <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image="/screen.jpg"
+        
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          URL Shortener
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          A simple URL shortener
+        </Typography>
+      </CardContent>
+      <CardActions className="cardbuttons">
+        <div className="code">
+        <Button size="small" onClick={() => {if(opt===1)setopt(0);
+          else setopt(1); }}><h3><AiFillGithub/></h3></Button>
+          <div className="code2">
+        {(opt===1)?(<Button size="small" onClick={() => {window.location.replace("https://github.com/sathwic-jain/URL-shortener") }}>Backend</Button>):<div></div>}
+        {(opt===1)?(<Button size="small" onClick={() => {window.location.replace("https://github.com/sathwic-jain/URL-shortener-frontend") }}>Frontend</Button>):<div></div>}
+        </div>
+        </div>
+        <Button size="small" onClick={() => {window.location.replace("https://flamboyant-snyder-59307d.netlify.app/") }}><h3><FaGlobe/></h3></Button>
+      </CardActions>
+    </Card>
+    </div>
+    <div className="card">
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image="/screen.jpg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Password Reset Flow
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          A simple Password reset app
+        </Typography>
+      </CardContent>
+      <CardActions className="cardbuttons">
+        <div className="code">
+        <Button size="small" onClick={() => {if(opt===2)setopt(0);
+          else setopt(2); }}><h3><AiFillGithub/></h3></Button>
+          <div className="code2">
+        {(opt===2)?(<Button size="small" onClick={() => {window.location.replace("https://github.com/sathwic-jain/Password") }}>Backend</Button>):<div></div>}
+        {(opt===2)?(<Button size="small" onClick={() => {window.location.replace("https://github.com/sathwic-jain/Password-reset-flow") }}>Frontend</Button>):<div></div>}
+        </div>
+        </div>
+        <Button size="small" onClick={() => {window.location.replace("https://nervous-pare-98939b.netlify.app/") }}><h3><FaGlobe/></h3></Button>
+      </CardActions>
+    </Card>
+      </div>
+      <div className="card">
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image="/screen.jpg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          CRM APP
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          A basic CRM app
+        </Typography>
+      </CardContent>
+      <CardActions className="cardbuttons">
+        <div className="code">
+        <Button size="small" onClick={() => {if(opt===3)setopt(0);
+          else setopt(3); }}><h3><AiFillGithub/></h3></Button>
+          <div className="code2">
+        {(opt===3)?(<Button size="small" onClick={() => {window.location.replace("https://github.com/sathwic-jain/Hackathon-2-CRM") }}>Backend</Button>):<div></div>}
+        {(opt===3)?(<Button size="small" onClick={() => {window.location.replace("https://github.com/sathwic-jain/CRM") }}>Frontend</Button>):<div></div>}
+        </div>
+        </div>
+        <Button size="small" onClick={() => {window.location.replace("https://objective-wozniak-8e7e8e.netlify.app/") }}><h3><FaGlobe/></h3></Button>
+      </CardActions>
+    </Card>
+      </div>
+      <div className="card">
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image="/screen.jpg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Shopping cart
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          A simple frontend working shopping cart
+        </Typography>
+      </CardContent>
+      <CardActions className="cardbuttons">
+        <div className="code">
+        <Button size="small" onClick={() => {if(opt===4)setopt(0);
+          else setopt(4); }}><h3><AiFillGithub/></h3></Button>
+          <div className="code2">
+        {(opt===4)?(<Button size="small" onClick={() => {window.location.replace("https://github.com/sathwic-jain/Cart_add") }}>Frontend</Button>):<div></div>}
+        </div>
+        </div>
+        <Button size="small" onClick={() => {window.location.replace("https://ecstatic-haibt-4a0606.netlify.app/") }}><h3><FaGlobe/></h3></Button>
+      </CardActions>
+    </Card>
+      </div>
+      <div className="card">
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image="/screen.jpg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          User CRUD app
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          A basic User CRUD app
+        </Typography>
+      </CardContent>
+      <CardActions className="cardbuttons">
+        <div className="code">
+        <Button size="small" onClick={() => {if(opt===5)setopt(0);
+          else setopt(5); }}><h3><AiFillGithub/></h3></Button>
+          <div className="code2">
+        {(opt===5)?(<Button size="small" onClick={() => {window.location.replace("https://github.com/sathwic-jain/CRUD-USERS-using-api") }}>Frontend</Button>):<div></div>}
+        </div>
+        </div>
+        <Button size="small" onClick={() => {window.location.replace("https://frosty-benz-75e937.netlify.app/") }}><h3><FaGlobe/></h3></Button>
+      </CardActions>
+    </Card>
+      </div>
+      <div className="card">
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image="/screen.jpg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Tic Tac Toe game
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          A simple Tic Tac Toe game
+        </Typography>
+      </CardContent>
+      <CardActions className="cardbuttons">
+        <div className="code">
+        <Button size="small" onClick={() => {if(opt===6)setopt(0);
+          else setopt(6); }}><h3><AiFillGithub/></h3></Button>
+          <div className="code2">
+        {(opt===6)?(<Button size="small" onClick={() => {window.location.replace("https://github.com/sathwic-jain/Game") }}>Frontend</Button>):<div></div>}
+        </div>
+        </div>
+        <Button size="small" onClick={() => {window.location.replace("https://nifty-euclid-d5bcda.netlify.app/") }}><h3><FaGlobe/></h3></Button>
+      </CardActions>
+    </Card>
+      </div>
+    </div>
+  );
+}
